5fceab222f0f339d002c2e16d3930ad3c8d659db
Turhan <turhan.huseynov@gmail.com>
2019-07-10 22:05:52 +0300

    Added FontAwesome

--- evreka-frontend/src/App.js
+++ evreka-frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import DatePickers from './Datepickers';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMap } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
       <div>
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          <div>Map Icon</div>
+          <FontAwesomeIcon icon={faMap} />
           <DatePickers></DatePickers>
         </div>
 
